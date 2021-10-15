4c83a05460988de2ba336a4f34a6d53d4817d8f8
GavryshAlex <alexgavryshua@gmail.com>
2021-10-16 00:20:50 +0300

    Added work with correct.txt and not-correct.txt

--- Program.cs
+++ Program.cs
@@ -32,6 +32,18 @@ namespace TextAnalyzer
 			}
 			return str;
 		}
+		private static double TrustFactor(string str, Dictionary<string, int> counts)
+		{
+			str = GetLetters(str);
+			int n = str.Length - 1;
+			double trustFactor = 1.0;
+			for (int i = 0; i < n; i++)
+			{
+				if (counts["" + str[i] + str[i + 1]] > 0)
+					trustFactor *= Math.Pow(counts["" + str[i] + str[i + 1]], 1.0 / n);
+			}
+			return trustFactor;
+		}
 		static void Main()
 		{
 			string str = "";
@@ -39,7 +51,7 @@ namespace TextAnalyzer
 			string currnetDirectory = Environment.CurrentDirectory.Substring(0, Environment.CurrentDirectory.Length - 9);
 			str = ReadFromFile(currnetDirectory + @"text-for-learning.txt");
 			WriteLine($"Symbol count: {str.Length}");
-			str = GetLetters(str);// removes ',', ';' etc.
+			str = GetLetters(str);// removes ',', '.' etc.
 			// https://stackoverflow.com/questions/23453260/find-and-count-latter-pair-from-a-string-by-alphabetical-order
 			Dictionary<string, int> counts = new Dictionary<string, int>();
 			for (char ch1 = 'a'; ch1 <= 'z'; ch1 ++)
@@ -49,6 +61,11 @@ namespace TextAnalyzer
 					counts.Add(""+ch1+ch2, 0);
 				}
 			}
+			for (char ch = 'a'; ch <= 'z'; ch++)
+			{
+				counts.Add(" " + ch, 0);
+				counts.Add(ch + " ", 0);
+			}
 			int n = str.Length;
 			for (int i = 0; i < n - 1; i ++)
 			{
@@ -56,17 +73,21 @@ namespace TextAnalyzer
 				if (counts.ContainsKey(index))
 					counts[index]++;
 			}
+			
+			double minCorrect = double.MaxValue, maxWrong = 0/*double.MinValue*/;
+			string[] correct = File.ReadAllLines(currnetDirectory + @"correct.txt");
+			string[] wrong = File.ReadAllLines(currnetDirectory + @"not-correct.txt");
+			foreach (string s in correct)
+				minCorrect = Math.Min(minCorrect, TrustFactor(s, counts));
+			foreach (string s in wrong)
+				maxWrong = Math.Max(maxWrong, TrustFactor(s, counts));
+			double border = (minCorrect + maxWrong) / 2;
 			WriteLine("Input string:");
 			str = ReadLine();
 			str = GetLetters(str);
-			n = str.Length;
-			int countChars = n - str.Count(Char.IsWhiteSpace) - str.Split().Length; // length - number of spaces - number of words
-			double trustFactor = 1.0;
-			for (int i = 0; i < n - 1; i ++)
-				if (str[i] != ' ' && str[i+1] != ' ' && counts["" + str[i] + str[i + 1]] > 0)
-					trustFactor *= Math.Pow(counts["" + str[i] + str[i + 1]], 1.0/countChars);
+			double trustFactor = TrustFactor(str, counts);
 			WriteLine($"trustFactor = {trustFactor}");
-			if (trustFactor > 3000)
+			if (trustFactor >= border)
 				WriteLine("Correct");
 			else
 				WriteLine("Nonsense");
